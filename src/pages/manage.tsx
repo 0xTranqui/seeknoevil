d89df0472ce6cb58067b574c7c98d7e3dbe5d2d1
0xTranqui <thinkandchill11@gmail.com>
2023-04-25 16:58:20 -0400

    conditonal video/markdown rendering now works in feed + finalized nav + page route access control updates

--- src/pages/manage.tsx
+++ src/pages/manage.tsx
@@ -2,13 +2,11 @@
 
 import Head from 'next/head';
 import React from 'react';
-import { EditorContextProvider } from '../components/markdown/context/EditorContext';
-import EditorPage from '../components/markdown/pages/EditorPage/EditorPage';
 import { useChannelAdmins } from '../providers/ChannelAdminProvider';
 import { useAuth } from '../hooks/useAuth';
 
 
-export default function Create() {
+export default function Manage() {
 
   const { address } = useAuth()
   const userAddress = address ? address : null
@@ -17,17 +15,17 @@ export default function Create() {
   return (
     <div>
       <Head>
-        <title>create</title>
+        <title>manage</title>
       </Head>
       <>
       {userAddress !== admin1 && userAddress !== admin2 ? (
         <div className="flex flex-row w-full h-[100vh] justify-center items-center">
-          you do not have access to "create"
+          you do not have access to "manage"
         </div>
       ) : (
-        <EditorContextProvider>
-        <EditorPage />
-      </EditorContextProvider>
+        <div className="flex flex-row w-full h-[100vh] justify-center items-center">
+            Manage functionality coming soon
+        </div>
       )}
       </>
     </div>
