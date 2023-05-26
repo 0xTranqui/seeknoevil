390660bd250985af21f2ed9080cda8d7d42ae5f1
0xTranqui <thinkandchill11@gmail.com>
2023-05-25 23:19:11 -0400

    add ens resolution to title creator title video pages

--- src/pages/[listing].tsx
+++ src/pages/[listing].tsx
@@ -220,7 +220,13 @@ const ListingPage: NextPage = () => {
                   href={`https://goerli.etherscan.io/address/${author}`}
                   className="font-[helvetica] hover:underline"
                 >
-                  {shortenAddress(author)}
+                  {
+                    !!author
+                    ? !!resolvedAuthor
+                      ? resolvedAuthor
+                      : shortenAddress(author)
+                    : "unknown"
+                  }
                 </a>
                 &nbsp;{"– " + publicationDate}
               </div>
