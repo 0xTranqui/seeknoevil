2a8f1dc67c2a9c32147748f479029c1c0cd6b378
0xTranqui <thinkandchill11@gmail.com>
2023-05-26 19:13:49 -0400

    rely on cloudflare content type headers for determing file type in media type check instead of magic number

--- src/pages/[listing].tsx
+++ src/pages/[listing].tsx
@@ -34,22 +34,13 @@ const ListingPage: NextPage = () => {
       if (!response.ok) {
         throw new Error("Failed to fetch media");
       }
-      const buffer = await response.arrayBuffer();
-      const uint8Array = new Uint8Array(buffer);
-      // Check for MP4 format
-      const magicNumber = uint8Array.slice(0, 8);
-      const mp4Signature = [0x00, 0x00, 0x00, 0x66, 0x74, 0x79, 0x70];
-      const isMp4 =
-        magicNumber
-          .slice(0, 3)
-          .every((value, index) => value === mp4Signature[index]) &&
-        magicNumber
-          .slice(4, 8)
-          .every((value, index) => value === mp4Signature[index + 3]);
-      if (isMp4) {
+      const contentType = response.headers.get('content-type');
+      if (contentType && contentType.includes('video/mp4')) {
         return "video";
       }
       // Check for markdown
+      const buffer = await response.arrayBuffer();
+      const uint8Array = new Uint8Array(buffer);
       const textDecoder = new TextDecoder();
       const textContent = textDecoder.decode(uint8Array);
       if (hasMarkdownPattern(textContent)) {
@@ -103,7 +94,6 @@ const ListingPage: NextPage = () => {
     }
   };
 
-
   const collectButtonContent =
     numMinted > 0
       ? "Collected!"
@@ -130,7 +120,7 @@ const ListingPage: NextPage = () => {
 
   const publicationDate = tokenCreated ? convertDate(tokenCreated) : "mm/dd/yy"
 
-    const [mediaType, setMediaType] = useState(null);
+  const [mediaType, setMediaType] = useState(null);
 
   useEffect(() => {
     (async () => {
@@ -173,6 +163,9 @@ const ListingPage: NextPage = () => {
             </a>
             &nbsp;{"– " + publicationDate}
           </div>
+          <div className="font-[helvetica] text-[14px] mt-[19px] mb-[60px] sm:mb-[19px] font-normal">
+            {description}
+          </div>            
           {ipfsPath && <MarkdownViewer ipfsPath={ipfsPath} />}
           <ListingInfo
             collectionAddress={process.env.NEXT_PUBLIC_AP_1155_CONTRACT}
